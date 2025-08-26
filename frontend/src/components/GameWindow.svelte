fbc04d28d65538833c1c2686ca328a5eb20a5c00
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2025-08-25 22:05:19 -0400

    centered the user and prevented the user from going out of bounds on mouse move and adjusting window size at max position

--- frontend/src/components/GameWindow.svelte
+++ frontend/src/components/GameWindow.svelte
@@ -1,19 +1,26 @@
 <script>
 	import { onMount } from 'svelte';
 
-	const xAxisMaxDelta = 15;
+	const INTERNAL_PADDING = 30; // px
+	const X_AXIS_MAX_DELTA = 15; // px
+	const USER_HITBOX_WIDTH = 64; // px
+	const USER_HITBOX_HEIGHT = 64; // px
 
 	let xAxisCurrentInterval;
 	let yAxisCurrentInterval;
 	let xAxisMouseIntervalId;
 
-	let mousePositionX = $state(0);
-	let userPositionX = $state(0);
+	let mousePositionX = $state(undefined);
+	let userPositionX = $state(undefined);
 	let userPositionY = $state(0);
 	let userFacingDirection = $state('right');
 	let gameWindowDimensions = $state({
 		width: 0,
 		height: 0,
+		minXAxisValue: 0,
+		maxXAxisValue: 0,
+		minYAxisValue: 0,
+		maxYAxisValue: 0,
 	});
 	let gameWindowRef = $state();
 
@@ -21,13 +28,25 @@
 	// onmount here used to prevent hot reloading from stacking multiple intervals
 	onMount(() => {
 		xAxisMouseIntervalId = setInterval(() => {
-			if (Math.abs(mousePositionX - userPositionX) <= xAxisMaxDelta) {
-				userPositionX = mousePositionX; // close enough, snap to target
+			if (userPositionX === undefined || mousePositionX == undefined) return;
+
+			if (userPositionX < gameWindowDimensions.minXAxisValue) {
+				mousePositionX = userPositionX = gameWindowDimensions.minXAxisValue;
+			} else if (
+				userPositionX >
+				gameWindowDimensions.maxXAxisValue - USER_HITBOX_WIDTH
+			) {
+				mousePositionX = userPositionX =
+					gameWindowDimensions.maxXAxisValue - USER_HITBOX_WIDTH;
 			} else {
-				if (mousePositionX < userPositionX) {
-					userPositionX -= xAxisMaxDelta;
-				} else if (mousePositionX > userPositionX) {
-					userPositionX += xAxisMaxDelta;
+				if (Math.abs(mousePositionX - userPositionX) <= X_AXIS_MAX_DELTA) {
+					userPositionX = mousePositionX; // close enough, snap to target
+				} else {
+					if (mousePositionX < userPositionX) {
+						userPositionX -= X_AXIS_MAX_DELTA;
+					} else if (mousePositionX > userPositionX) {
+						userPositionX += X_AXIS_MAX_DELTA;
+					}
 				}
 			}
 		}, 16);
@@ -42,7 +61,14 @@
 		const handleMouseMove = (e) => {
 			const rect = gameWindowRef.getBoundingClientRect();
 			// X position relative to the element
-			mousePositionX = e.clientX - rect.left;
+			const mousePositionXTemp = e.clientX - rect.left - USER_HITBOX_WIDTH / 2;
+			if (
+				mousePositionXTemp >= gameWindowDimensions.minXAxisValue &&
+				mousePositionXTemp <=
+					gameWindowDimensions.maxXAxisValue - USER_HITBOX_WIDTH
+			) {
+				mousePositionX = mousePositionXTemp;
+			}
 		};
 
 		gameWindowRef.addEventListener('mousemove', handleMouseMove);
@@ -56,11 +82,19 @@
 	onMount(() => {
 		if (!gameWindowRef) return;
 
+		// initialize userPositionX to the center
+		userPositionX =
+			gameWindowRef.getBoundingClientRect().width / 2 - USER_HITBOX_WIDTH / 2;
+
 		const updateDimensions = () => {
 			const rect = gameWindowRef.getBoundingClientRect();
 			gameWindowDimensions = {
 				width: rect.width,
 				height: rect.height,
+				minXAxisValue: INTERNAL_PADDING,
+				maxXAxisValue: rect.width - INTERNAL_PADDING,
+				minYAxisValue: INTERNAL_PADDING,
+				maxYAxisValue: rect.height - INTERNAL_PADDING,
 			};
 		};
 
@@ -83,8 +117,8 @@
 		class="border border-gray-300 rounded-lg h-full w-full relative"
 	>
 		<div
-			class="absolute rounded-full bg-gray-500 h-16 w-16"
-			style="bottom: 0; left:{userPositionX}px"
+			class="absolute rounded-full bg-gray-500"
+			style="height: {USER_HITBOX_HEIGHT}px; width: {USER_HITBOX_WIDTH}px; bottom: 0; left:{userPositionX}px"
 		></div>
 	</div>
 </div>
